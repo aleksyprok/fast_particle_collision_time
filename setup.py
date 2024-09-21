80fa9a7bb19ffbd01121db97e50ab6a77e05928b
Alex Prokopyszyn <aleksyprok@gmail.com>
2024-09-21 22:26:53 +0100

    Added setup.py so python package can be installed using pip install -e .

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""
+This is the setup script for the codebase.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fast_particle_collision_time',
+    version='0.1.0',
+    packages=find_packages(),
+)
