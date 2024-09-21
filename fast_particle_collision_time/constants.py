70798091333599740564d4c2841955d19571a24e
Alex Prokopyszyn <aleksyprok@gmail.com>
2024-09-21 23:31:33 +0100

    Added coulomb log and permitivity of free space constants.

--- fast_particle_collision_time/constants.py
+++ fast_particle_collision_time/constants.py
@@ -4,3 +4,7 @@ Constants used across the codebase.
 
 # Electron charge in Coulombs
 ELECTRON_CHARGE = 1.60217662e-19
+# Coulomb logarithm (dimensionless)
+COULOMB_LOG = 17
+# Permittivity of free space in F/m
+PERMITTIVITY_OF_FREE_SPACE = 8.8541878128e-12
