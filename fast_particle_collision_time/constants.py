7c288b99822386a3a6bc9e9394fb3099243a967a
Alex Prokopyszyn <aleksyprok@gmail.com>
2024-09-22 10:40:51 +0100

    Added some extra constants to constants.py

--- fast_particle_collision_time/constants.py
+++ fast_particle_collision_time/constants.py
@@ -1,6 +1,7 @@
 """
 Constants used across the codebase.
 """
+import numpy as np
 
 # Electron charge in Coulombs
 ELECTRON_CHARGE = 1.60217662e-19
@@ -8,3 +9,9 @@ ELECTRON_CHARGE = 1.60217662e-19
 COULOMB_LOG = 17
 # Permittivity of free space in F/m
 PERMITTIVITY_OF_FREE_SPACE = 8.8541878128e-12
+# Electron mass in kg
+ELECTRON_MASS = 9.10938356e-31
+# Proton mass in kg
+PROTON_MASS = 1.672621777e-27
+# Alpha ion mass in kg
+ALPHA_ION_MASS = 6.644656e-27
