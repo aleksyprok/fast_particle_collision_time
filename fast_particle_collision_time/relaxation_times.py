a5a791475e1748c3c1a703659a9a81199738afad
Alex Prokopyszyn <aleksyprok@gmail.com>
2024-09-22 10:37:20 +0100

    Added relaxation_times and tested the slowing down calculation.

---
+++ fast_particle_collision_time/relaxation_times.py
@@ -0,0 +1,122 @@
+"""
+This module contains functions for calculating the slowing down and deflection times
+of a test particle interacting with a Maxwellian background species.
+
+Please note that we follow the equations 2.14.1 and 2.14.2 in the book:
+Wesson, J. Tokamaks, Vol. 149. Oxford University Press, 2011. 
+"""
+
+import numpy as np
+import scipy.special
+from fast_particle_collision_time import constants
+
+def _a_d(background_density, test_charge_state, background_charge_state, test_mass):
+    """
+    Calculates the A_D function (see page 63 of Wesson, 2011).
+
+    Parameters
+    ----------
+    background_density : float
+        The background density of the background species
+        in m^(-3)
+    test_charge_state : float
+        The charge state of the test particle (dimensionless)
+    background_charge_state : float
+        The charge state of the background species (dimensionless)
+    test_mass : float
+        The mass of the test particle in kg
+
+    Returns
+    -------
+    float
+        The value of A_D in m^3 s^(-4)
+    """
+    return background_density * constants.ELECTRON_CHARGE**4 * test_charge_state**2 \
+           * background_charge_state**2 * constants.COULOMB_LOG / \
+           (2 * np.pi * constants.PERMITTIVITY_OF_FREE_SPACE**2 * test_mass**2)
+
+def _phi(x):
+    """
+    Calculates the Phi function on page 62 of Wesson, 2011. Note that it is
+    equivalent to the error function.
+    """
+    return scipy.special.erf(x)
+
+def _phi_prime(x):
+    """
+    Calculates the derivative of the Phi function on page 62 of Wesson, 2011. Note that it is
+    equivalent to the derivative of the error function.
+    """
+    return 2 * np.exp(-x**2) / np.sqrt(np.pi)
+
+def _psi(x):
+    """
+    Calculates the Psi function on page 63 of Wesson, 2011.
+    """
+    return (_phi(x) - x * _phi_prime(x)) / (2 * x**2)
+
+def slowing_down_time(test_speed, background_thermal_speed, test_mass, background_mass,
+                      background_density, test_charge_state, background_charge_state):
+    """
+    Calculates the slowing down time (see page Eq. 2.14.1 of Wesson, 2011).
+
+    Parameters
+    ----------
+    test_speed : float
+        The speed of the test particle in m s^(-1)
+    background_thermal_speed : float
+        The thermal speed of the background species in m s^(-1)
+    test_mass : float
+        The mass of the test particle in kg
+    background_mass : float
+        The mass of the background species in kg
+    background_density : float
+        The background density of the background species
+        in m^(-3)
+    test_charge_state : float
+        The charge state of the test particle (dimensionless)
+    background_charge_state : float
+        The charge state of the background species (dimensionless)
+
+    Returns
+    -------
+    float
+        The value of the slowing down time in s
+    """
+    return 2 * background_thermal_speed**2 * test_speed / \
+           ((1 + test_mass / background_mass) * \
+            _a_d(background_density, test_charge_state, background_charge_state, test_mass) \
+            * _psi(test_speed / (np.sqrt(2) * background_thermal_speed)))
+
+def deflection_time(test_speed, background_thermal_speed, test_mass, background_mass,
+                    background_density, test_charge_state, background_charge_state):
+    """
+    Calculates the deflection time (see page Eq. 2.14.2 of Wesson, 2011).
+
+    Parameters
+    ----------
+    test_speed : float
+        The speed of the test particle in m s^(-1)
+    background_thermal_speed : float
+        The thermal speed of the background species in m s^(-1)
+    test_mass : float
+        The mass of the test particle in kg
+    background_mass : float
+        The mass of the background species in kg
+    background_density : float
+        The background density of the background species
+        in m^(-3)
+    test_charge_state : float
+        The charge state of the test particle (dimensionless)
+    background_charge_state : float
+        The charge state of the background species (dimensionless)
+
+    Returns
+    -------
+    float
+        The value of the deflection time in s
+    """
+    return test_speed**3 / \
+           (_a_d(background_density, test_charge_state, background_charge_state, test_mass) * \
+            (_phi(test_speed / (np.sqrt(2) * background_thermal_speed)) - \
+             _psi(test_speed / (np.sqrt(2) * background_thermal_speed))))
---
+++ tests/test_relaxtion_times.py
@@ -0,0 +1,89 @@
+"""
+Tests for the relaxation_times module.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+from fast_particle_collision_time import constants, relaxation_times
+
+def test_slowing_down_time_alpha_vs_electrons():
+    """
+    Test slowing down time for an alpha particle in an electron background.
+    The electrons are assumed to have a temperature of 10 keV and density of 10^20 m^-3.
+    We compare the result of the relaxation_times.slowing_down_time to the approximation
+    given in Eq. 5.4.3 for speeds up to 3.5 MeV.
+    """
+    # Parameters
+    electron_temperature_ev = 10e3  # 10 keV electron temperature
+    electron_temperature_j = electron_temperature_ev * constants.ELECTRON_CHARGE # Joules
+    electron_density = 1e20  # m^-3
+    alpha_mass = constants.ALPHA_ION_MASS  # kg
+    electron_mass = constants.ELECTRON_MASS  # kg
+    alpha_charge_state = 2  # charge state of alpha
+    electron_charge_state = 1  # charge state of electron
+
+    # Thermal speed of background electrons
+    electron_thermal_speed = np.sqrt(electron_temperature_j / electron_mass)
+
+    # Speeds of the alpha particle from 0 to 3.5 MeV
+    alpha_energies_mev = np.linspace(0.01, 3.5, 100)  # MeV
+    alpha_energies_joules = alpha_energies_mev * 1e6 * constants.ELECTRON_CHARGE
+    alpha_speeds = np.sqrt(alpha_energies_joules / alpha_mass)  # m/s
+
+    def slowing_down_time_approx(t_e, n_e, m_e, m_alpha, z_e, z_alpha):
+        """
+        Analytical approximation for the slowing down time given in Eq. 5.4.3.
+        in Wesson (2011).
+        """
+        def a_d(n_e, z_e, z_alpha, m_alpha):
+            return n_e * constants.ELECTRON_CHARGE**4 * constants.COULOMB_LOG \
+                   * z_e**2 * z_alpha**2 / \
+                   (2 * np.pi * constants.PERMITTIVITY_OF_FREE_SPACE**2 * m_alpha**2)
+
+        return 3 * np.sqrt(2 * np.pi * t_e**3) / \
+            (np.sqrt(m_e) * m_alpha * a_d(n_e, z_e, z_alpha, m_alpha))
+
+    # Values for the slowing down time
+    tau_slowing_down_times = []
+    tau_slowing_down_approx = []
+
+    # Calculate the slowing down time for each alpha speed
+    for v_alpha in alpha_speeds:
+        tau_s = relaxation_times.slowing_down_time(v_alpha, electron_thermal_speed, alpha_mass,
+                                                   electron_mass, electron_density,
+                                                   alpha_charge_state, electron_charge_state)
+        tau_approx = slowing_down_time_approx(electron_temperature_j, electron_density,
+                                              electron_mass, alpha_mass,
+                                              electron_charge_state, alpha_charge_state)
+
+        tau_slowing_down_times.append(tau_s)
+        tau_slowing_down_approx.append(tau_approx)
+
+    # Check the specific case where energy is 3.5 MeV
+    tau_s_3_5_mev = tau_slowing_down_times[-1]
+    tau_approx_3_5_mev = tau_slowing_down_approx[-1]
+
+    # Ensure the values are approximately equal within a tolerance
+    assert tau_s_3_5_mev == pytest.approx(tau_approx_3_5_mev, rel=0.1), \
+        f"Slowing down time at 3.5 MeV differs: tau_s = {tau_s_3_5_mev}," \
+        f" tau_approx = {tau_approx_3_5_mev}"
+
+    # Plot the results for visual inspection
+    fig, ax = plt.subplots()
+    ax.plot(alpha_energies_mev, tau_slowing_down_times,
+            label="Numerical slowing down time (Eq. 2.14.1)")
+    ax.plot(alpha_energies_mev, tau_slowing_down_approx,
+            label="Analytical approximation (Eq. 5.4.3)", linestyle="--")
+    ax.set_xlabel("Alpha Energy (MeV)")
+    ax.set_ylabel("Slowing Down Time (s)")
+    ax.set_title("Slowing Down Time of Alpha Particle in Electron Background (10 keV, 10^20 m^-3)")
+    ax.legend()
+    ax.grid(True)
+
+    # Save the plot for inspection
+    fig.savefig("slowing_down_time_alpha_vs_electrons.png",
+                dpi=300, bbox_inches="tight")
+
+if __name__ == "__main__":
+    test_slowing_down_time_alpha_vs_electrons()
